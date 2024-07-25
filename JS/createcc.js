6fbb9dc7ef824dd3f99bc22720f8ddd896466e4f
vvguerravv <victor.eg@aluno.ifsc.edu.br>
2024-07-24 21:39:52 -0300

    Iniciando JS

---
+++ JS/createcc.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form')
+const email = document.getElementById('createEmailInput')
+const passoword = document.getElementById('createPasswordInput')
+const confirmPassoword = document.getElementById('confirmPasswordInput')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault;
+})
