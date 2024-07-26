1b7cb912f1299f1de58b5547e88f7ddbf9843311
Viquetti1 <leviquetti@gmail.com>
2024-07-26 18:12:31 -0300

    teste

--- JS/createcc.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('form')
-const email = document.getElementById('createEmailInput')
-const passoword = document.getElementById('createPasswordInput')
-const confirmPassoword = document.getElementById('confirmPasswordInput')
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault;
-})
