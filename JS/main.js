babf2f101c0fe1388c38121c33b5bc0c5a2a71b0
Vinicius Prass <88332258+viniciusprass@users.noreply.github.com>
2024-08-01 17:57:40 -0300

    update

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,3 @@
-var overlay = document.querySelector('#overlay')
-
-window.addEventListener('load', function() {
-    var video = document.querySelector('#background-video');
-
-    video.muted = true;
-    video.loop = true;
-});
 document.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     if (window.scrollY > 0) {
@@ -17,45 +9,45 @@ document.addEventListener("scroll", function() {
 
 
 //Animação da logo
-var logo = document.querySelector('#logo-header');
-var words = ["Ngg", "NggFLix", "moovie","BrasFlix","ClipCloud", "Undefined"];
-var currentWordIndex = 0;
+// var logo = document.querySelector('#logo-header');
+// var words = ["Ngg", "NggFLix", "moovie","BrasFlix","ClipCloud", "Undefined"];
+// var currentWordIndex = 0;
 
-function animateText() {
-    var currentWord = words[currentWordIndex];
-    var text = currentWord.substring(0, logo.textContent.length + 1); 
+// function animateText() {
+//     var currentWord = words[currentWordIndex];
+//     var text = currentWord.substring(0, logo.textContent.length + 1); 
 
-    logo.textContent = text;
-    if (currentWord === "Undefined"){
-        logo.style.color = '#fff'
-        logo.style.textShadow = '0 0 30px #fff'
-    } else {
-        logo.style.color = 'rgba(255, 255, 255, .4)'
-        logo.style.textShadow = 'none'
-        logo.style.transition = '0s'
-    }
-    if (text === currentWord) {
-        if (currentWord === "Undefined"){
-            setTimeout(eraseText, 10000);
-        } else {
-            setTimeout(eraseText, 1000);
-        }
-    } else {
-        setTimeout(animateText, 80);
-    }
+//     logo.textContent = text;
+//     if (currentWord === "Undefined"){
+//         logo.style.color = '#fff'
+//         logo.style.textShadow = '0 0 30px #fff'
+//     } else {
+//         logo.style.color = 'rgba(255, 255, 255, .4)'
+//         logo.style.textShadow = 'none'
+//         logo.style.transition = '0s'
+//     }
+//     if (text === currentWord) {
+//         if (currentWord === "Undefined"){
+//             setTimeout(eraseText, 10000);
+//         } else {
+//             setTimeout(eraseText, 1000);
+//         }
+//     } else {
+//         setTimeout(animateText, 80);
+//     }
 
-}
+// }
 
-function eraseText() {
-    var text = logo.textContent;
-    logo.textContent = text.substring(0, text.length - 1);
+// function eraseText() {
+//     var text = logo.textContent;
+//     logo.textContent = text.substring(0, text.length - 1);
 
-    if (text.length === 0) {
-        currentWordIndex = (currentWordIndex + 1) % words.length;
-        setTimeout(animateText, 500);
-    } else {
-        setTimeout(eraseText, 50);
-    }
-}
+//     if (text.length === 0) {
+//         currentWordIndex = (currentWordIndex + 1) % words.length;
+//         setTimeout(animateText, 500);
+//     } else {
+//         setTimeout(eraseText, 50);
+//     }
+// }
 
-animateText();
+//animateText();
